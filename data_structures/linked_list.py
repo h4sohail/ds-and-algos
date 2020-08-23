326fdf9dd5113599db4ff02707c7149502c9e8d4
Hamza Sohail <hamzasohail@Hamzas-MacBook-Pro.local>
2020-08-23 11:09:03 -0400

    Update linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,48 +1,46 @@
-class Node:
-    def __init__(self, data=None, next=None): 
-        self.data = data
-        self.next = next
+class Node():
+    def __init__(self, _data=None, _next=None):
+        self.data = _data
+        self.next = _next
+    
 
-class LinkedList:
-    def __init__(self):  
+class LinkedList():
+    def __init__(self):
         self.head = None
-  
+
     def __str__(self):
-        node = self.head
         nodes = []
-        while(node != None):
-            nodes.append(str(node.data))
-            node = node.next
-        nodes.append('None')
-        return '->'.join(nodes)
+        current = self.head
+        while(current != None):
+            if (current.data != None):
+                nodes.append(current.data)
+            current = current.next
+        nodes.append("None")
+        return "->".join(nodes)
 
     def insert(self, data):
         node = Node(data)
-        if(self.head):
+        if self.head != None:
             current = self.head
-            while(current.next):
+            while(current.next != None):
                 current = current.next
             current.next = node
         else:
             self.head = node
-
-    def reverse(self): 
+    
+    def reverse(self):
+        current = self.head
         prev = None
-        current = self.head 
-        while(current != None): 
+        while(current != None):
             next_node = current.next
             current.next = prev
-            prev = current 
+            prev = current
             current = next_node
         self.head = prev
+            
 
-if __name__ == '__main__':
-    node_data = ['a', 'b', 'c']
-    linked_list = LinkedList()
-
-    for data in node_data:
-        linked_list.insert(data)
-    
-    print(linked_list)
-    linked_list.reverse()
-    print(linked_list)
+ll = LinkedList()
+for item in ['a', 'b', 'c']:
+    ll.insert(item)
+ll.reverse()
+print(ll)
