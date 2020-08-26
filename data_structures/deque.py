d08c16681e8c8b31c4c3c2e28423076f9bd7e545
Hamza Sohail <hamzasohail@Hamzas-MacBook-Pro.local>
2020-08-26 16:56:46 -0400

    Update deque

--- data_structures/deque.py
+++ data_structures/deque.py
@@ -1,7 +1,6 @@
 class Deque():
     def __init__(self):
         self._deque = []
-        self._count = 0
 
     def __str__(self):
         return str(self._deque)
@@ -9,32 +8,21 @@ class Deque():
     def __len__(self):
         return len(self._deque)
 
-    def count(self):
-        return self._count
-
     def push_back(self, item):
-        self._count += 1
         self._deque.append(item)
         return item
 
     def push_front(self, item):
-        self._count += 1
         self._deque.insert(0, item)
         return item
 
     def pop_back(self):
-        if self.count() > 0:
-            self._count -= 1
-            temp = self._deque[-1]
-            del self._deque[-1]
-            return temp
+        if len(self._deque) > 0:
+            return self._deque.pop()
 
     def pop_front(self):
-        if self.count() > 0:
-            self._count -= 1
-            temp = self._deque[0]
-            del self._deque[0]
-            return temp
+        if len(self._deque) > 0:
+            return self._deque.pop(0)
 
     def contains(self, item):
         return item in self._deque
@@ -43,5 +31,4 @@ class Deque():
         return [item for item in self._deque]
 
     def clear(self):
-        self._count = 0
-        del self._deque[:]
+        self._deque = []
